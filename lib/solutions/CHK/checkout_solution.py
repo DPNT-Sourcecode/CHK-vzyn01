17c5b8647452309b3e592f6c4504ed689cea97c5
sbx_user1051 <sbx_user1051@169.254.6.93>
2024-07-22 05:20:09 +0000

    Mon Jul 22 05:20:09 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if isalpha(skus):
+        a_total = skus.count('A')//3 * 130 + skus.count('A') % 3 * 50
+        b_total = skus.count('B')//2 * 45 + skus.count('A') % 2 * 30
+        c_total = skus.count('C') * 20
+        d_total = skus.count('D') * 15
+
+        return a_total + b_total + c_total + d_total
+        #Treat any other alphabets as bad barcode reads for now
+    else:
+        #Characters other than alphabets in string
+        return -1
+
