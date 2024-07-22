08a8514560cbe073232d49092241f5cc975b4c82
sbx_user1051 <sbx_user1051@169.254.6.93>
2024-07-22 05:05:52 +0000

    Mon Jul 22 05:05:52 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if isinstance(x, int) and isinstance(y,int) and 0<= x <=100 and 0 <= y <=100:
+        return x+y
+    else:
+        return -1
+
