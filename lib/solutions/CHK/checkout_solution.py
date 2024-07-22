9375dd2b1447940d381a2549479b59364180f191
sbx_user1051 <sbx_user1051@169.254.6.93>
2024-07-22 05:25:41 +0000

    Mon Jul 22 05:25:41 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,15 +2,17 @@
 # skus = unicode string
 def checkout(skus):
     if skus.isalpha():
+        skus.upper()
         a_total = skus.count('A')//3 * 130 + skus.count('A') % 3 * 50
-        b_total = skus.count('B')//2 * 45 + skus.count('A') % 2 * 30
+        b_total = skus.count('B')//2 * 45 + skus.count('B') % 2 * 30
         c_total = skus.count('C') * 20
         d_total = skus.count('D') * 15
 
         return a_total + b_total + c_total + d_total
         #Treat any other alphabets as bad barcode reads for now
+    elif skus == "":
+        return 0
     else:
         #Characters other than alphabets in string
         return -1
-    
-    return -1
+
