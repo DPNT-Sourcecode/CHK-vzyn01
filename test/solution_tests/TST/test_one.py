3efe26c749ef271028e6972071912098ff6bcf39
sbx_user1051 <sbx_user1051@169.254.6.93>
2024-07-22 07:14:59 +0000

    Mon Jul 22 07:14:59 UTC 2024

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -3,5 +3,6 @@ from solutions.CHK import checkout_solution
 
 class TestOne():
     def test_one(self):
-        assert checkout_solution.checkout(['SSSX']) == 62
+        assert checkout_solution.checkout('SSSX') == 62
+
 
