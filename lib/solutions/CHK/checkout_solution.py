24b873598b22ab80ef80fec4cc52577f75ee8c75
sbx_user1051 <sbx_user1051@169.254.6.93>
2024-07-22 05:50:00 +0000

    Mon Jul 22 05:50:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,22 +9,25 @@ def checkout(skus):
         c_count = skus.count('C')
         d_count = skus.count('D')
         e_count = skus.count('E')
+        f_count = skus.count('F')
 
-        if a_count + b_count + c_count + d_count + e_count < len(skus):
+        if a_count + b_count + c_count + d_count + e_count + f_count < len(skus):
             return -1
         
         a_total = a_count//5 * 200 + (a_count%5)//3 * 130 + (a_count%5) % 3 * 50
         c_total = c_count * 20
         d_total = d_count * 15
         e_total = e_count * 40
+        f_total = f_count//3 * 20 + f_count % 3 * 10
 
         b_count = b_count - e_count // 2
         if b_count < 0:
             b_count = 0
         b_total = b_count//2 * 45 + b_count % 2 * 30
 
-        return a_total + b_total + c_total + d_total + e_total
+        return a_total + b_total + c_total + d_total + e_total + f_total
     
     #Characters other than alphabets in string
     return -1
+
 
